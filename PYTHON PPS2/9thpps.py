2b5c8f507851ef954c19b2beafbb41069470f309
Vrishank Pandit <121352696+vrishankpandit@users.noreply.github.com>
2023-01-05 21:23:25 +0530

    updated 9th pps

--- PYTHON PPS2/9thpps.py
+++ PYTHON PPS2/9thpps.py
@@ -1,7 +1,15 @@
+# x=input()
+# b=x.replace('a',"")
+# c=b.replace('e',"")
+# d=c.replace('i',"")
+# e=d.replace('o',"")
+# f=e.replace('u',"")
+# print(f)
+
 x=input()
-b=x.replace('a',"")
-c=b.replace('e',"")
-d=c.replace('i',"")
-e=d.replace('o',"")
-f=e.replace('u',"")
-print(f)
+vowels="aeiouAEIOU"
+output=""
+for ch in x:
+  if ch not in vowels:
+    output+=ch
+print(output)
