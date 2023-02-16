113b9192fa65ed497b8838434b3b5cb3573f4b50
VISHU <vrishankpandit1344@gmail.com>
2023-02-16 08:31:18 +0530

    DEEP AND SHALLOW COPY

--- OOPS/85OOPSconcepts.cpp
+++ OOPS/85OOPSconcepts.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<algorithm>
+#include <cstring>
 using namespace std;
 
 class hero{
@@ -7,24 +9,40 @@ class hero{
 private:
 int health;
 public:
+char *name;
 char level;
 
 //
-// hero(){
-//     cout<<"constructor called"<<endl;
-// }
-
-//parametrized constructor
-hero(int health){
-    this->health=health;
-    cout<<"address of ramesh is "<<this<<endl;
+hero(){
+    cout<<"constructor called"<<endl;
+    name=new char[100];
 }
 
+// //parametrized constructor
+// hero(int health){
+//     this->health=health;
+//     cout<<"address of ramesh is "<<this<<endl;
+// }
 
+//COPY CONSTRUCTOR
+
+hero(hero& temp){
+// deep copy
+char *ch=new char[strlen(temp.name)+1];
+strcpy(ch,temp.name);
+this->name=ch;
+
+cout<<"copy constructor called "	<<endl;
+this->health=temp.health;
+this->level=temp.level;
+
+}
 
 void print(){
     // cout<<health<<endl;
-    cout<<level<<endl;
+    cout<<"name is "<<this->name	<<endl;
+    cout<<"health is "<<this->health	<<endl;
+    cout<<"level is "<<this->level	<<endl;
 }
 
 int gethealth(){
@@ -33,6 +51,10 @@ int gethealth(){
 
 char getlevel(){
     return level;
+}
+
+void setname(char name[]){
+    strcpy(this->name,name);
 }
 
 void sethealth(int h){
@@ -46,6 +68,19 @@ void setlevel(char ch){
 };
 
 int main(){
+
+    hero hero1;
+    hero1.sethealth(70);
+    hero1.setlevel(70);
+    char name[7]="suiiii";
+    hero1.setname(name);
+    hero hero2(hero1);
+    hero1.print();
+
+    hero1.name[2]='o';
+
+    hero2.print();
+
 
     //dynamic allocation
     // hero *b=new hero;
@@ -67,8 +102,8 @@ int main(){
     // // cout<<ramesh.health<<endl;
     // cout<<ramesh.level<<endl;
 
-    hero ramesh(10);
-    cout<<"address of ramesh "<<&ramesh<<endl;
+    // hero ramesh(10);
+    // cout<<"address of ramesh "<<&ramesh<<endl;
     
     return 0;
 }
